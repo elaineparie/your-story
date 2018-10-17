71d27bb1dff8bc44ce58bbd90e9dcdd2a0f29e71
github username <github email address>
2018-10-17 17:14:54 -0400

    backend auth complete

--- your-story-api/app/controllers/api/v1/auth_controller.rb
+++ your-story-api/app/controllers/api/v1/auth_controller.rb
@@ -1,13 +1,14 @@
 module Api
   module V1
     class AuthController < ApplicationController
+      skip_before_action :authorized, only: [:create]
 
       def create
         user = User.find_by(:email => params[:email])
-      if user && user.authenticate(params[:password])
+        if user && user.authenticate(params[:password])
         token = encode_token({ user_id: user.id })
          render json: { user: user, jwt: token}
-       end
+        end
       end
 
 
